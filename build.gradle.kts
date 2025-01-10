f56a4632df0e91a3afb19bac7e6d53d35a10a48c
Hunter104 <000plagueinc@gmail.com>
2025-01-10 13:23:27 -0300

    adicionar configuração incial do bot

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.2.0")
 }
 
 tasks.test {
--- src/main/kotlin/org/example/Main.kt
+++ src/main/kotlin/org/example/Main.kt
@@ -1,6 +1,17 @@
 package org.example
 
+import com.github.kotlintelegrambot.bot
+import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.text
+import com.github.kotlintelegrambot.entities.ChatId
 
 fun main() {
-    println("Hello World!")
+    val bot = bot {
+        token  = System.getenv("BOT_TOKEN")
+        dispatch {
+            text { bot.sendMessage(ChatId.fromId(message.chat.id), text = "Hello World!") }
+        }
+    }
+
+    bot.startPolling()
 }
